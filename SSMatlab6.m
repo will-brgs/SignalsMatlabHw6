9ece29bdd00f432c2fb948ba4af5564c4c61411f
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-04-04 19:38:05 -0500

    balls

--- SSMatlab6.m
+++ SSMatlab6.m
@@ -9,23 +9,33 @@ clear
 clc
 %% Part  1: Create Binary PAM System
 %% Generate Input Signal and Add Noise Factor
-Fs = 10;
-maxTime = 2;
+Tp = 0.1;
+dt = Tp/50;
+
+Fs = 1/dt;
+maxTime = Tp;
 sampleTimes = 0:1/Fs:maxTime;
 
 x = round(rand(1,length(sampleTimes)));
 x(x==0) = -1;
 n = randn(1,length(x));
 
-sigma = 0.1;
+sigma = 1;
 
 n = n * sigma;
 
-r = x .* n;
+r = x + n;
+
+rect = ones(1,50);
+
+pulse = 2 * conv(rect, rect);
+
+pulse_fft = fft(pulse);
 
 figure, hold on
-subplot(3,1,1),plot(x);
-subplot(3,1,2),plot(n);
-subplot(3,1,3),plot(r); 
+% subplot(3,1,1),stem(x);
+% subplot(3,1,2),plot(n);
+% subplot(3,1,3),plot(r); 
+stem(pulse);
 hold off
 %% Part 2: Performance Test
