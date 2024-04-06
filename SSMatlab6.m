cde078cfbac60de8d972244dec00e5bc7cbd3724
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-04-06 16:47:39 -0500

    pee

--- SSMatlab6.m
+++ SSMatlab6.m
@@ -9,55 +9,57 @@ clear
 clc
 %% Part  1: Create Binary PAM System
 %% Generate Input Signal and Add Noise Factor
-Tp = 0.1;
-Ts = 100;
-dt = Tp/50;
+Tp = 0.1; % Half pulse width
+sample_period = Tp/50; % dt, pulse and recieve sample period
+sample_freq = 1/sample_period; % Frequency of pulse and recieve signal 
 
-Fs = 1/dt;
+bit_rate = 1/(Tp); %Fb, frequency of bits sent out
+bit_period = 1/bit_rate; % Ts, Time between bits sent out
+
 maxTime = Tp;
-sampleTimes = 0:1/Fs:maxTime;
+sampleTimes = 0:sample_period:maxTime;
 
 x = round(rand(1,length(sampleTimes)));
 x(x==0) = -1;
 n = randn(1,length(x));
 
 rect = ones(1,50);
-
 pulse = 2 * conv(rect, rect);
-
 pulse_fft = fftshift(pulse);
 
-figure, hold on
+%figure, hold on
 % subplot(3,1,1),stem(x);
 % subplot(3,1,2),plot(n);
 % subplot(3,1,3),plot(r); 
 %stem(pulse);
-plot(mag2db(abs(pulse_fft)));
-hold off
+%plot(mag2db(abs(pulse_fft)));
+%hold off
 
 N = 20;
+
+
+
 xn = 2 * ((rand(1, N) > 0.5) - 0.5);
 a = 0;
-imp_train = zeros(1,N*Ts);
+imp_train = zeros(1,N * sample_freq * bit_period);
 for k = 1:length(imp_train)
-    if mod(k - 1, Ts) == 0
-    a = a+1;
+    if mod(k - 1, sample_freq * bit_period) == 0
+    a = a + 1;
     imp_train(k) = xn(a);
     else
     imp_train(k) = 0;    
     end
 end
 
-
 y = conv(imp_train,pulse);
 % figure, subplot (2,1,1),plot(y)
-% subplot(2,1,2),plot(xn)
+% subplot(2,1,2),stem(xn)
 
 sigma = 10;
-noise = sigma*randn(1,length(y));
+noise = sigma * randn(1,length(y));
 r = y + (noise * sigma);
 
-figure,plot(r)
+%figure,plot(r)
 %% Part f:  Create various outputs
 
 % i, p(t), p(w)
@@ -83,4 +85,5 @@ xlabel('Index')
 title('Transmittetd Signal y(t)')
 
 % iii, r(t)
+
 %% Part 2: Performance Test
